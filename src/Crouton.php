cd5f1a698bad6377abbee24e3872e68ba7762c54
ddymko <dymkod@gmail.com>
2016-08-17 20:50:08 -0400

    I remember my first git commit

---
+++ src/Crouton.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kaktus\Crouton;
+
+
+class Crouton
+{
+    public function __construct()
+    {
+
+    }
+
+    public function test()
+    {
+        echo "test";
+    }
+}
---
+++ tests/CroutonTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dymko
+ * Date: 8/17/16
+ * Time: 8:09 PM
+ */
+
+namespace Kaktus\Tests;
+
+
+class CroutonTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
